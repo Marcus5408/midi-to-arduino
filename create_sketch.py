1ee0c343b30aadb3c139be4545cdab13f7b5be92
Issac Liu <issac@matchaflavored.site>
2024-12-10 23:30:20 -0500

    Add note limit check in modify_melody function to prevent making an array that won't fit

--- create_sketch.py
+++ create_sketch.py
@@ -8,8 +8,16 @@ def modify_melody(file_path, new_melody):
         for line in lines:
             if line.strip().startswith("const PROGMEM int melody["):
                 num_notes = len(new_melody)
+                note_limit = 4677
+                if num_notes > note_limit:
+                    print(f"[WARN] Number of notes is {num_notes}, {num_notes-note_limit} greater than {note_limit} limit.")
+                    print(f"[INFO] Writing only first {note_limit} notes.")
+                    num_notes = note_limit
                 file.write(f"const PROGMEM int melody[{num_notes}][3] = {{\n")
-                for note, duration, velocity in new_melody:
+                for index, note_info in enumerate(new_melody):
+                    if index >= num_notes:
+                        break
+                    note, duration, velocity = note_info
                     file.write(f"  {{ {note}, {duration}, {velocity} }},\n")
             else:
                 file.write(line)
